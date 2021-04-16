e982c803cd558c57297510d91e7c06d10d2c4e8e
EC2 Default User <ec2-user@ip-172-31-31-243.ap-northeast-1.compute.internal>
2021-04-16 08:10:03 +0000

    ページネーションを使用

--- cms/app/Http/Controllers/BooksController.php
+++ cms/app/Http/Controllers/BooksController.php
@@ -12,7 +12,7 @@ use Auth;       //認証モデルを使用する
 class BooksController extends Controller{
     //本ダッシュボード表示
     public function index() {
-        $books = Book::orderBy('created_at', 'asc')->get();
+        $books = Book::orderBy('created_at', 'asc')->paginate(3);
         return view('books', [
             'books' => $books
         ]);
